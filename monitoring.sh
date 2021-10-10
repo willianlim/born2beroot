31d6e4aeb9ead97edcf39866f28fc390e2d99180
Willian-code <willianrosendodelima@outlook.com>
2021-10-09 21:54:15 -0300

    test27

--- monitoring.sh
+++ monitoring.sh
@@ -22,10 +22,10 @@ df -h | awk '$NF=="/"{printf("%d/%dGB (%s)\n"), $3, $2, $5}'
 
 # The current utilization rate of your processors as a percentage
 echo -n "#CPU load: "
-top -b -n1 | grep ^%Cpu | awk '{printf("%.2f%%\n"), 100-&8}'
+top -b -n1 | grep ^%Cpu | awk '{printf("%.2f%%\n"), 100-$8}'
 
 # The date and time of the last reboot
-echo -n "Last boot: "
+echo -n "#Last boot: "
 who -b | awk '{printf("%s %s\n"), $3, $4}'
 
 # Whether LVM is active or not
@@ -34,16 +34,25 @@ if lsblk | grep -q "lvm";then echo "yes";else echo "no";fi
 
 # The number of active connections
 echo -n "#Connexions TCP : "
-ss -s | awk 'NR == 7{printf("%d ESTABLISHED\n"), $3}'
+#echo -n $(ss -s | awk 'NR == 7 {printf("%d ESTABLISHED\n"), $3}')
+echo -n "ESTABLISHED " && netstat -tnpa | grep 'ESTABLISHED.*sshd' | wc -l
 
 # The number of users using the server
 echo -n "#User log: "
 users | wc -w
 
+
+IP=$(hostname -I)
+MAC=$(cat /sys/class/net/enp0s3/address)
 # The IPv4 address of your server and its MAC (Media Access Control) address
-echo -n "#Network: "
-echo -n "IP " && hostname -I | grep "." | awk '{print $1}' | tr '\n' ' ' && ip link | grep "link/ether" | awk '{printf("(%s)\n"), $2}'
+echo -n "#Network: IP ${IP}"
+echo "(${MAC})"
+#hostname -I | awk '{printf("IP %s\n"), $1}' | tr '\n' ' ' && ip link | grep "link/ether" | awk '{printf("(%s)\n"), $2}'
+#hostname -I | awk '{printf("IP %s"), $1}'
+#hostname -I | awk '{printf("IP %s "), $1}' && ip link show | grep "link/ether" | awk '{printf("(%s)\n"), $2}'
+#hostname -I | awk '{printf("IP %s "), $1}' && ethtool -P enp0s3
+#hostname -I | grep '.' | tr '\n' ' ' && ethtool -P enp0s3
 
 # The number of commands executed with the sudo program
 echo -n "#Sudo : "
-cat /var/log/sudo.log | wc -l | tr '\n' ' ' && echo "cmd"
+cat /var/log/sudo/sudo.log | wc -l | tr '\n' ' ' && echo "cmd"
